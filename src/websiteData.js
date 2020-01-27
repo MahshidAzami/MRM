77ec088441735dc45b3f41469e516469fb5d97bf
Mahshid Azami <m.azami2008@gmail.com>
2020-01-27 10:39:37 +0000

    hello world

---
+++ src/websiteData.js
@@ -0,0 +1,343 @@
+let data = {
+  navbarItems: [
+    {
+      name: "Home"
+    },
+    {
+      name: "Services",
+      subMenu: [
+        "HandyMan",
+        "Electrics",
+        "Home Makeover",
+        "Carpentry",
+        "Plumbing",
+        "Moving Services",
+        "Decorating",
+        "Jet Washing",
+        "Locksmith"
+      ]
+    },
+    {
+      name: "Prices"
+    },
+    {
+      name: "About Us"
+    }
+  ],
+  landingPage: {
+    banner: {
+      src: "",
+      alt: ""
+    },
+    heroText: "blah blah blah",
+    describeSection: {
+      heading: "blah blah 1",
+      text:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+      image: {
+        src: "",
+        alt: ""
+      }
+    }
+  },
+  servicesSection: {
+    heading: "Our Services",
+    category: [
+      {
+        name: "HandyMan",
+        link: "handyMan",
+        describtion:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+        image: {
+          src: "",
+          alt: ""
+        },
+        services: [
+          {
+            name: "subservice1",
+            text: "text for subservice1",
+            image: {
+              src: "",
+              alt: ""
+            },
+            link: "",
+            subServices: [
+              {
+                name: "subservice1",
+                text: "text for subservice1",
+                image: {
+                  src: "",
+                  alt: ""
+                },
+                link: ""
+              }
+            ]
+          }
+        ]
+      },
+      {
+        name: "Electrics",
+        link: "electrics",
+        describtion:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+        image: {
+          src: "",
+          alt: ""
+        },
+        services: [
+          {
+            name: "subservice1",
+            text: "text for subservice1",
+            image: {
+              src: "",
+              alt: ""
+            },
+            link: "",
+            subServices: [
+              {
+                name: "subservice1",
+                text: "text for subservice1",
+                image: {
+                  src: "",
+                  alt: ""
+                },
+                link: ""
+              }
+            ]
+          }
+        ]
+      },
+      {
+        name: "Home Makeover",
+        link: "home-makeover",
+        describtion:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+        image: {
+          src: "",
+          alt: ""
+        },
+        services: [
+          {
+            name: "subservice1",
+            text: "text for subservice1",
+            image: {
+              src: "",
+              alt: ""
+            },
+            link: "",
+            subServices: [
+              {
+                name: "subservice1",
+                text: "text for subservice1",
+                image: {
+                  src: "",
+                  alt: ""
+                },
+                link: ""
+              }
+            ]
+          }
+        ]
+      },
+      {
+        name: "Carpentry",
+        link: "carpentry",
+        describtion:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+        image: {
+          src: "",
+          alt: ""
+        },
+        services: [
+          {
+            name: "subservice1",
+            text: "text for subservice1",
+            image: {
+              src: "",
+              alt: ""
+            },
+            link: "",
+            subServices: [
+              {
+                name: "subservice1",
+                text: "text for subservice1",
+                image: {
+                  src: "",
+                  alt: ""
+                },
+                link: ""
+              }
+            ]
+          }
+        ]
+      },
+      {
+        name: "Plumbing",
+        link: "plumbing",
+        describtion:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+        image: {
+          src: "",
+          alt: ""
+        },
+        services: [
+          {
+            name: "subservice1",
+            text: "text for subservice1",
+            image: {
+              src: "",
+              alt: ""
+            },
+            link: "",
+            subServices: [
+              {
+                name: "subservice1",
+                text: "text for subservice1",
+                image: {
+                  src: "",
+                  alt: ""
+                },
+                link: ""
+              }
+            ]
+          }
+        ]
+      },
+      {
+        name: "Moving Services",
+        link: "moving-services",
+        describtion:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+        image: {
+          src: "",
+          alt: ""
+        },
+        services: [
+          {
+            name: "subservice1",
+            text: "text for subservice1",
+            image: {
+              src: "",
+              alt: ""
+            },
+            link: "",
+            subServices: [
+              {
+                name: "subservice1",
+                text: "text for subservice1",
+                image: {
+                  src: "",
+                  alt: ""
+                },
+                link: ""
+              }
+            ]
+          }
+        ]
+      },
+      {
+        name: "Decorating",
+        link: "decorating",
+        describtion:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+        image: {
+          src: "",
+          alt: ""
+        },
+        services: [
+          {
+            name: "subservice1",
+            text: "text for subservice1",
+            image: {
+              src: "",
+              alt: ""
+            },
+            link: "",
+            subServices: [
+              {
+                name: "subservice1",
+                text: "text for subservice1",
+                image: {
+                  src: "",
+                  alt: ""
+                },
+                link: ""
+              }
+            ]
+          }
+        ]
+      },
+      {
+        name: "Jet Washing",
+        link: "jet-washing",
+        describtion:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+        image: {
+          src: "",
+          alt: ""
+        },
+        services: [
+          {
+            name: "subservice1",
+            text: "text for subservice1",
+            image: {
+              src: "",
+              alt: ""
+            },
+            link: "",
+            subServices: [
+              {
+                name: "subservice1",
+                text: "text for subservice1",
+                image: {
+                  src: "",
+                  alt: ""
+                },
+                link: ""
+              }
+            ]
+          }
+        ]
+      },
+      {
+        name: "Locksmith",
+        link: "locksmith",
+        describtion:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+        image: {
+          src: "",
+          alt: ""
+        },
+        services: [
+          {
+            name: "subservice1",
+            text: "text for subservice1",
+            image: {
+              src: "",
+              alt: ""
+            },
+            link: "",
+            subServices: [
+              {
+                name: "subservice1",
+                text: "text for subservice1",
+                image: {
+                  src: "",
+                  alt: ""
+                },
+                link: ""
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  prices: {
+    title: "Our Rates",
+    subHeader: "blah blah blah",
+    text:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate ",
+    tableStandard: {}
+  }
+};
